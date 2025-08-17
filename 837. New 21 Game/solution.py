321ad1b2893719a1cf70e6e540eba79cd9f610ee
Ankit Gupta <ankitgupta18k@gmail.com>
2025-08-17 23:13:14 +0530

    Update solution.py

--- 837. New 21 Game/solution.py
+++ 837. New 21 Game/solution.py
@@ -1,1 +1,10 @@
-
+class Solution:
+    def new21Game(self, n: int, k: int, maxPts: int) -> float:
+        dp: list[float] = [0] * (n + 1)
+        dp[0] = 1
+        cur_prob: float = int(bool(k))
+        for i in range(1, n + 1):
+            dp[i] = cur_prob / maxPts
+            if i < k: cur_prob += dp[i]
+            if i - maxPts >= 0 and i - maxPts < k: cur_prob -= dp[i - maxPts]
+        return sum(dp[k:])
