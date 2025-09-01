6f8bc62879648a731a8cc990c425db13662a2cc4
Ankit Gupta <ankitgupta18k@gmail.com>
2025-09-01 22:00:30 +0530

    Create solution.py

---
+++ 1792. Maximum Average Pass Ratio/solution.py
@@ -0,0 +1,18 @@
+import heapq
+
+class Solution:
+    def maxAverageRatio(self, classes, extraStudents: int) -> float:
+        def gain(pass_count, total_count):
+            return (pass_count + 1) / (total_count + 1) - pass_count / total_count
+
+        heap = [(-gain(p, t), p, t) for p, t in classes]
+        heapq.heapify(heap)
+
+        while extraStudents > 0:
+            g, p, t = heapq.heappop(heap)
+            p += 1
+            t += 1
+            heapq.heappush(heap, (-gain(p, t), p, t))
+            extraStudents -= 1
+
+        return sum(p / t for _, p, t in heap) / len(classes)
